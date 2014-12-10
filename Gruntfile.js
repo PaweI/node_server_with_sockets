5ff52b36feca1b692dadd7ce05276aeeb121017f
Pavel Redics <pavel.redics@gmail.com>
2014-12-10 15:17:44 +0000

    Watch checks if server and test files being changed, and reruns them again

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,21 +21,43 @@ module.exports = function(grunt) {
         },
       },
     },
+    express: {
+      options: {
+        port: 3000,
+      },
+      dev: {
+        options: {
+          script: 'path/to/dev/server.js'
+        }
+      },
+      prod: {
+        options: {
+          script: 'path/to/prod/server.js',
+          node_env: 'production'
+        }
+      },
+      test: {
+        options: {
+          script: 'server.js'
+        }
+      }
+    },
     watch: {
       scripts: {
-        files: ['test/*.js'],
-        tasks: ['jshint'],
+        files: ['server.js', 'test/**/*.js'],
+        tasks: ['express:test', 'mocha_casperjs'],
         options: {
           spawn: false,
         },
       },
-    }
+    },
   });
 
 
   grunt.loadNpmTasks('grunt-mocha-casperjs');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-express-server');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['mocha_casperjs', 'jshint', 'watch']);
+  grunt.registerTask('default', ['express:test', 'mocha_casperjs']);
 };
